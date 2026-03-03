6be4b41c51e544c80c5dcfcbaf779a26c07dbf11
priyansh3010 <pri.obhan30@gmail.com>
2026-03-03 14:32:53 +0530

    Added square enums and updated existing. Added constants.

--- types.h
+++ types.h
@@ -3,5 +3,19 @@
 
 typedef uint64_t U64;
 
-enum class Piece { King, Queen, Rook, Bishop, Knight, Pawn };
-enum class Color { White, Black };
+enum Piece { KING, QUEEN, ROOK, BISHOP, KNIGHT, PAWN };
+enum Color { WHITE, BLACK, ALL };
+
+enum Square {
+    a1, b1, c1, d1, e1, f1, g1, h1,
+    a2, b2, c2, d2, e2, f2, g2, h2,
+    a3, b3, c3, d3, e3, f3, g3, h3,
+    a4, b4, c4, d4, e4, f4, g4, h4,
+    a5, b5, c5, d5, e5, f5, g5, h5,
+    a6, b6, c6, d6, e6, f6, g6, h6,
+    a7, b7, c7, d7, e7, f7, g7, h7,
+    a8, b8, c8, d8, e8, f8, g8, h8
+};
+
+const int NUM_SQUARES = 64;
+const int NUM_UNIQUE_PIECES = 6;
