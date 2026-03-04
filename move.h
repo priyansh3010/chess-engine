3baf3e5641c4670405a2676cda41b9b1f26e9a32
priyansh3010 <pri.obhan30@gmail.com>
2026-03-04 16:05:19 +0530

    Updated move struct for better future development.

---
+++ move.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "types.h"
+
+struct Move {
+    int fromSquare;
+    int toSquare;
+    Piece pieceType;
+    Piece capturedPiece;
+    Piece promotionPiece;
+    bool isCastle;
+    bool isEnPassant;
+
+    Move(int from, int to, Piece piece, Piece captured = NONE, 
+         Piece promotion = NONE, bool castle = false, bool enPassant = false)
+        : fromSquare(from), toSquare(to), pieceType(piece),
+          capturedPiece(captured), promotionPiece(promotion),
+          isCastle(castle), isEnPassant(enPassant) {}
+};
