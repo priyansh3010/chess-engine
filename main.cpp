915be57888dbfaecaef1dfa8c0c8cb4e296e6fd2
priyansh3010 <pri.obhan30@gmail.com>
2026-04-26 13:50:49 +0530

    Removed unnecessary comments, making main file a lot simpler.

--- main.cpp
+++ main.cpp
@@ -11,27 +11,7 @@
 using namespace std;
 
 int main() {
-    // Your main logic goes here
-
-    /*
-    Board board;
-    board.init();
-    // board.printBoard();
-    
-    MoveGen::init();
-    
-    auto start = chrono::high_resolution_clock::now();
-    int nodes = perftDivide(board, 5);
-    auto end = chrono::high_resolution_clock::now();
-    
-    board.printBoard();
-    cout << "End of program" << endl;
-    
-    double seconds = std::chrono::duration<double>(end - start).count();
-    std::cout << "NPS: " << (uint64_t)(nodes / seconds) << std::endl;
-    */
-
     UCI::loop();
-
+    
     return 0;
 }
